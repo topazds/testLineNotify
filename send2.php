410cb66690a208ccdf65b4d60c41ded5408c55f8
topazds <top.p_1507@windowslive.com>
2018-01-24 18:11:04 +0700

    Update send2.php

--- send2.php
+++ send2.php
@@ -1,27 +1,3 @@
 <?php
-//Get POST body content 
-// $content = file_get_contents('php://input');
-// //Parse JSON 
-// $events = json_decode($content,true);
-//$title = $events['payload']['title']
-// $message = $events['payload']['title']
-$message = "มันทรมานในหัวเอยยยยยยย หัวใจจจจจจ"
-$token = 'VG10VkKCRJ2Gy3BFEb6TNJKAvNa8arkXvFLeWjSIzyy';
-echo send_line_notify($message, $token);
-function send_line_notify($message, $token)
-{
-  $ch = curl_init();
-  curl_setopt( $ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
-  curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
-  curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt( $ch, CURLOPT_POST, 1);
-  curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
-  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-  $headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", );
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-  $result = curl_exec( $ch );
-  curl_close( $ch );
-  return $result;
-}
+echo "test";
 ?>
